edf3e60e4382073ef7a4efcb36b5bd43721db0c7
jedmonson <jedmonson1478@gmail.com>
2021-09-25 20:32:47 -0400

    updated instance commands, refactored music manager

--- src/Alfred.js
+++ src/Alfred.js
@@ -2,23 +2,6 @@ const discord = require('discord.js');
 const client = new discord.Client();
 const ActionHandler = require("./actionHandler");
 const fetch = require('node-fetch');
-const { v4: uuidv4 } = require('uuid');
 
-async function run(identifier) {
-    IP = "http://localhost:"
-    PORT = "8081"
-    PATH = "/manager/child"
+const ah = new ActionHandler(new discord.Client())
 
-    let address = IP+PORT+PATH
-    let body = {"instance": identifier}
-    const resp = await fetch(address, {method: 'POST',
-        body: JSON.stringify(body),
-	    headers: {'Content-Type': 'application/json'}})
-    if (resp.status === 200) {
-        console.log("Manager Active!")
-        const ah = new ActionHandler(new discord.Client(), identifier, IP, PORT)
-    }
-
-}
-
-run(uuidv4().split('-')[0])
--- src/musicManager.js
+++ src/musicManager.js
@@ -1,134 +1,163 @@
 const fetch = require("node-fetch");
+const { v4: uuidv4 } = require('uuid');
 
-musicQueue = [];
-isPlaying = false;
-connection = false;
-dispatcher = false;
+class MusicManager {
+    #guild = '';
+    #id = '';
+    #musicQueue = [];
+    #isPlaying = false;
+    #connection = false;
+    #dispacher = false;
 
-async function verifyAvailable(identifier, managerPath){
-    console.log(" ## I am free for jobs...")
-    let resp = await fetch(managerPath+"/manager/available", {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            "instance": identifier
-            },
-        )});
-    if (resp.status === 200){
+    constructor(guild, message) {
+        this.#guild = guild;
+        this.#id = uuidv4().split('-')[0]
+        this.sendInitial(message)
+    }
+
+    getGuild(){
+        return this.#guild;
+    }
+
+    getID(){
+        return this.#id;
+    }
+
+    sendInitial(message) {
+        message.channel.send(`Alfred#${this.#id} ALPHA has been created!`);
+    }
+
+    // entry point to playing audio
+    async play(message){
+        if (this.#isPlaying){
+            let trackName = await this.getTrackName(message.content)
+            this.#musicQueue.push(message);
+            message.channel.send(`Adding ${trackName} to the queue.`);
+            console.log(`Adding ${trackName} to the queue.`)
+        } else {
+            this.#connection = await message.member.voice.channel.join();
+            console.log(`connection ${this.#connection}`)
+            await this.playTrack(message, await this.download(message.content))
+        };
         return true;
-    };
-    return false;
-}
-
-// entry point to playing audio
-async function play(message, identifer, managerPath){
-    if (isPlaying){
-        trackName = await getTrackName(message.content)
-        musicQueue.push(message);
-        message.channel.send(`Adding ${trackName} to the queue.`);
-        console.log(`Adding ${trackName} to the queue.`)
-    } else {
-        connection = await message.member.voice.channel.join();
-        await playTrack(message, connection, identifer, managerPath, await download(message.content))
-    };
-    return true;
-    
-}
-
-// stopping all music, clearing queue.
-async function stop(message, identifier, managerPath){
-    musicQueue = [];
-    connection.disconnect();
-    isPlaying = false;
-    console.log("Queue Cleared.")
-    await verifyAvailable(identifier, managerPath)
-};
-
-// skipping track, calling next track
-async function skip(message, identifier){
-    nextTrack(message, connection, identifier);
-};
-
-// pause track
-async function pause(message){
-    if (dispatcher){
-        dispatcher.pause();
+        
     }
-};
 
-// resume track
-async function resume(message){
-    if (dispatcher){
-        dispatcher.resume();
+    // stopping all music, clearing queue.
+    async stop(){
+        this.#musicQueue = [];
+        this.#connection.disconnect();
+        this.#isPlaying = false;
+        console.log("Queue Cleared.")
+    };
+
+    // skipping track, calling next track
+    async skip(message){
+        await this.nextTrack(message);
+    };
+
+    // pause track
+    async pause(){
+        if (this.#dispacher){
+            this.#dispacher.pause();
+        }
+    };
+
+    // resume track
+    async resume(){
+        if (this.#dispacher){
+            this.#dispacher.resume(true);
+        }
+    };
+
+    // download track using python package
+    async download(content){
+        let resp = await fetch("http://localhost:8080/alfred/track", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({"link": content},
+            )});
+        return resp;
+    };
+
+    // get track name
+    async getTrackName(content){
+        let resp = await fetch("http://localhost:8080/alfred/trackname", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({"link": content},
+            )});
+        let respJson = await resp.json();
+        return respJson['track'];
+    };
+
+    // connect to message originating guild channel, play if 200 status from
+    // python downloader.
+    async playTrack(message, resp) {
+        console.log(process.cwd())
+        if (resp.status == 200) {
+            let respJson = await resp.json()
+            let track = respJson['track']
+            console.log(`playing ${track}`)
+            this.#dispacher = this.#connection.play(`music/${track}`,  {bitrate: 256000 /* 192kbps */});
+            this.#dispacher.on('start', () => {
+                message.channel.send(`Alfred#${this.getID()} playing ${respJson['track']}`);
+                console.log('Music is Playing');
+                this.#isPlaying = true;
+            });
+            this.#dispacher.on('finish', async () => {
+                await this.nextTrack(message)
+            });
+        } else {
+            this.#dispacher = this.#connection.play('audio/error.mp3')
+            this.#dispacher.on('start', () => {
+                console.log('Error');
+                this.#isPlaying = true;
+            });
+            this.#dispacher.on('finish', async () => {
+                await this.nextTrack(message)
+            });
+        };
+    };
+
+    // check music queue length, if there is another track to play download
+    // it and play it.
+    async nextTrack(message) {
+        if (this.#musicQueue.length != 0) {
+            console.log('songs exist in queue');
+            let nextTrack = this.#musicQueue[0];
+            this.#musicQueue.splice(0,1);
+            // !!! Need to fix to actually display track name !!!
+            message.channel.send(`Alfred#${this.getID()} attempting to fetch track in queue.`);
+            await this.playTrack(message, await this.download(nextTrack.content));
+        } else {
+            console.log('queue is clear');
+            this.stop(message)
+        };
+    };
+
+    async handle(message){
+        if (message.content.includes('aplay')){
+            this.play(message);
+        };
+        if (message.content.includes('astop')){
+            this.stop(message);
+        };
+
+        if (message.content.includes('askip')){
+            this.skip(message);
+        };
+
+        if (message.content.includes('apause')){
+            this.pause(message);
+        };
+
+        if (message.content.includes('aresume')){
+            this.resume(message);
+        };
     }
+
+
 };
 
-// download track using python package
-async function download(content){
-    resp = await fetch("http://localhost:8080/alfred/track", {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({"link": content},
-        )});
-    return resp;
-};
-
-// get track name
-async function getTrackName(content){
-    resp = await fetch("http://localhost:8080/alfred/trackname", {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({"link": content},
-        )});
-    respJson = await resp.json();
-    return respJson['track'];
-};
-
-// connect to message originating guild channel, play if 200 status from
-// python downloader.
-async function playTrack(message, connection, identifier, managerPath, resp) {
-    console.log(process.cwd())
-    if (resp.status == 200) {
-        respJson = await resp.json()
-        track = respJson['track']
-        console.log(`playing ${track}`)
-        dispatcher = connection.play(`music/${track}`,  {bitrate: 256000 /* 192kbps */});
-        dispatcher.on('start', () => {
-            message.channel.send(`Alfred#${identifier} playing ${respJson['track']}`);
-            console.log('Music is Playing');
-            isPlaying = true;
-        });
-        dispatcher.on('finish', async () => {
-            await nextTrack(message, connection, identifier, managerPath)
-        });
-    } else {
-        dispatcher = connection.play('audio/error.mp3')
-        dispatcher.on('start', () => {
-            console.log('Error');
-            isPlaying = true;
-        });
-        dispatcher.on('finish', async () => {
-            await nextTrack(message, connection, identifier, managerPath)
-        });
-    };
-};
-
-// check music queue length, if there is another track to play download
-// it and play it.
-async function nextTrack(message, connection, identifier, managerPath) {
-    if (musicQueue.length != 0) {
-        console.log('songs exist in queue');
-        let nextTrack = musicQueue[0];
-        musicQueue.splice(0,1);
-        // !!! Need to fix to actually display track name !!!
-        message.channel.send(`Alfred#${identifier} attempting to fetch track in queue.`);
-        await playTrack(message, connection, identifier, managerPath, await download(nextTrack.content));
-    } else {
-        console.log('queue is clear');
-        stop(message, identifier, managerPath)
-    };
-};
-
-
-
-module.exports = {play, stop, skip, pause, resume};
+module.exports = MusicManager;
